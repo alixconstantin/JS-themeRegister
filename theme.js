dfab25b33498e1ce4e79eeaff2afc3d052d660d1
alixconstantin <64416579+alixconstantin@users.noreply.github.com>
2021-10-06 11:45:25 +0200

    up

--- theme.js
+++ theme.js
@@ -3,10 +3,10 @@ let texte = document.querySelector("span");
 
 
 // Si l'utilisateur possède en cookie theme = sombre en chargeant la page
-    if(localStorage.getItem('theme') == ('sombre')) {
-      // Applique le mode sombre
-      modeSombre(); 
-    }
+if (localStorage.getItem('theme') == ('sombre')) {
+    // Applique le mode sombre
+    modeSombre();
+}
 
 // Quand l'utilisateur clique sur le bouton, la fonction test est effectué
 button.addEventListener("click", test);
@@ -16,21 +16,19 @@ function test() {
 
     // Si l'élément DOM body contient dans sa liste de classe, la classe " dark ", la classe est retiré, le texte du bouton passe en theme sombre, et le cookie est créer avec la valeur clair
     if (document.body.classList.contains("dark")) {
-          document.body.classList.remove("dark");
+        document.body.classList.remove("dark");
         texte.innerText = "Thème sombre";
         localStorage.setItem("theme", "clair");
 
-    // Sinon, la fonction modeSombre est appeler
+        // Sinon, la fonction modeSombre est appeler
     } else {
         modeSombre();
     }
 }
 
-// Fonction modeSombre rajoute la class dark a l'élement body, le texte du bouton passe en mode theme claire, et le cookie est créer avec la valeur sombre
+// Fonction modeSombre rajoute la class dark a l'élement body, le texte du bouton passe en mode theme clair, et le cookie est créer avec la valeur sombre
 function modeSombre() {
-
     document.body.classList.add("dark");
     texte.innerText = "Thème clair";
     localStorage.setItem("theme", "sombre");
 }
-
